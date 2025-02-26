1b1c5992cba423fba6227956f97ea87b4157d069
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-26 22:35:36 +0000

    ajoute UI de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./Auth/register/register.component";
+import {LoginComponent} from "./Auth/login/login.component";
 
 export const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
 
+  { path: 'login', component: LoginComponent }
 
 
 ];
