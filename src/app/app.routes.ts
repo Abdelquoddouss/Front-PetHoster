559bfbf68297bf4eb08c1aa11d4e12987ce2c175
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-26 23:15:25 +0000

    ajoute UI de navbar dans layoute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./Auth/register/register.component";
 import {LoginComponent} from "./Auth/login/login.component";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
 
 export const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
 
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'nav', component: NavbarComponent }
 
 
 ];
