2d2499bb75dc8b687a49d3f03b0794a6a3a722bf
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-27 11:22:58 +0000

    ajoute Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,21 @@ import {RegisterComponent} from "./Auth/register/register.component";
 import {LoginComponent} from "./Auth/login/login.component";
 import {NavbarComponent} from "./navbar/navbar.component";
 import {FooterComponent} from "./footer/footer.component";
+import {LayoutComponent} from "./layout/layout.component";
 
 export const routes: Routes = [
 
-  { path: 'register', component: RegisterComponent },
 
-  { path: 'login', component: LoginComponent },
+  { path: 'footer', component: FooterComponent },
   { path: 'nav', component: NavbarComponent },
-  { path: 'footer', component: FooterComponent }
 
+  { path: '',
+    component: LayoutComponent,
+  children: [
+    { path: 'register', component: RegisterComponent },
+
+    { path: 'login', component: LoginComponent },
+  ]}
 
 
 ];
