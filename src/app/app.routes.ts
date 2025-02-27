b85bd668b245d35f0817e32587e9234266c7ab15
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-27 11:14:31 +0000

    ajoute UI de footers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./Auth/register/register.component";
 import {LoginComponent} from "./Auth/login/login.component";
-import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {FooterComponent} from "./footer/footer.component";
 
 export const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
 
   { path: 'login', component: LoginComponent },
-  { path: 'nav', component: NavbarComponent }
+  { path: 'nav', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent }
+
 
 
 ];
