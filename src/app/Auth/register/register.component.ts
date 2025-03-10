f438c14b60f9ab5df2f6746e73997f141ca9392a
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-03-10 15:17:27 +0000

    ajoute LOGIQUE register

--- src/app/Auth/register/register.component.ts
+++ src/app/Auth/register/register.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import { AuthService } from '../../services/auth.service';
+import {Router} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      nom: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      prenom: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      motDePasse: ['', [Validators.required, Validators.minLength(8)]],
+      telephone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      adresse: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const registerData = {
+        ...this.registerForm.value,
+        role: 'PROPRIETAIRE'
+      };
+
+      this.authService.register(registerData).subscribe({
+        next: (response) => {
+          console.log('Registration successful', response);
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          console.error('Registration failed', err);
+        }
+      });
+    }
+  }
 }
