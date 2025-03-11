38b403ccbaa6c88142b30629e55a0d3c0cb66357
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-03-11 00:16:26 +0000

    stocker user dans localstorage

--- src/app/Auth/login/login.component.ts
+++ src/app/Auth/login/login.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,
+  CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      this.authService.login(loginData).subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+          this.router.navigate(['/']); // Rediriger vers la page d'accueil après la connexion
+        },
+        error: (err) => {
+          console.error('Login failed', err);
+          // Afficher un message d'erreur à l'utilisateur
+          alert('Email ou mot de passe incorrect.');
+        }
+      });
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, tap} from 'rxjs';
 
 
 @Injectable({
@@ -13,5 +13,31 @@ export class AuthService {
 
   register(registerData: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/register`, registerData);
+  }
+
+  login(loginData: { email: string, password: string }): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/login`, loginData).pipe(
+      tap(response => {
+        // Stocker le token dans le localStorage
+        if (response.token) {
+          localStorage.setItem('authToken', response.token);
+        }
+      })
+    );
+  }
+
+  // Méthode pour vérifier si l'utilisateur est connecté
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('authToken');
+  }
+
+  // Méthode pour déconnecter l'utilisateur
+  logout(): void {
+    localStorage.removeItem('authToken');
+  }
+
+  // Méthode pour récupérer le token
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
   }
 }
